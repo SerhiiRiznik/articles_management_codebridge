c898beab481db36f81c93e40b3e402e739f1c81b
Serhii Riznyk <riznyk.serhii.v@gmail.com>
2021-11-22 17:01:29 +0200

    fixed bag

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const API_KEY = '185b4b74e0b94507a1cdb596fe63766f'
-
-
+let today = new Date();
+let dd = String(today.getDate()).padStart(2, '0');
+let mm = String(today.getMonth() + 1).padStart(2, '0');
+let yyyy = today.getFullYear();
 const instance = axios.create({
   baseURL: 'https://newsapi.org'  
 });
@@ -10,7 +12,7 @@ const instance = axios.create({
 class Api {
    
    static getArticles() {
-      return instance.get(`/v2/everything?q=tesla&from=2021-10-17&sortBy=publishedAt&apiKey=${API_KEY}`)
+      return instance.get(`/v2/everything?q=tesla&from=${yyyy}-${mm}-${dd}&sortBy=publishedAt&apiKey=${API_KEY}`)
    }
 }
 
